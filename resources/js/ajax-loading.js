6aedaaee06385b3970b2cdf2441435de1a5becc6
Pasajeromoronmoreno <pasajeromoronmoreno@gmail.com>
2016-08-17 10:59:04 -0300

    Funcionando Ajax sin login

--- resources/js/ajax-loading.js
+++ resources/js/ajax-loading.js
@@ -1,4 +1,4 @@
-// Load slide in body
+// Load slide in slide div in body
 $(function() {
     $.ajax('/pages/slide.html').done(function(result) {
         $('#slide').html(result);
@@ -16,5 +16,16 @@ $(function() {
 $(function() {
     $.ajax('/footer.html').done(function(result) {
         $("#footer").html(result);
+    });
+});
+
+// Carga cuando se hace click en algun item del menu
+$(document).on("click","#menu-principal li > a",function(e) {
+    $('#slide').remove();
+    e.preventDefault();
+    var url = $(this).attr("href");
+    // -- Cargo el contenido de la pagina en el div principal
+    $.ajax(url).done(function(result){
+        $("#content").html(result);
     });
 });
